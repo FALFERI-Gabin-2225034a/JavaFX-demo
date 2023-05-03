d0a0a557bff8269e676093162e5ce36c20a11da8
GabinFALFERIF22003745 <gabin.falferi@etu.univ-amu.fr>
2023-05-03 12:03:18 +0200

    Commencement Exercice 2

--- src/main/java/com/example/partie1/TicTacToe.java
+++ src/main/java/com/example/partie1/TicTacToe.java
@@ -2,21 +2,27 @@ package com.example.partie1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Création du conteneur principal
+        GridPane grille = new GridPane();
 
         // Ajout du conteneur à la scene
-        Scene scene = new Scene(vbox);
+        Scene scene = new Scene(grille);
 
         // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
         primaryStage.setScene( scene );
         primaryStage.setWidth( 800 );
         primaryStage.setHeight( 600 );
-        primaryStage.setTitle("Premier Exemple manipulant les conteneurs");
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setTitle("Tic Tac Toe");
 
         // Affichage de la fenêtre
         primaryStage.show();
