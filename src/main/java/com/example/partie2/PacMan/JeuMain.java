0f6f5a224cf1dba5fc542502040a0860a657cae5
GabinFALFERIF22003745 <gabin.falferi@etu.univ-amu.fr>
2023-05-10 12:12:59 +0200

    Création de l'appli PacMan

---
+++ src/main/java/com/example/partie2/PacMan/JeuMain.java
@@ -0,0 +1,96 @@
+package com.example.partie2.PacMan;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class JeuMain extends Application {
+
+    private Scene scene;
+    private BorderPane root;
+    private ArrayList<Obstacle> listObstacles;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        root = new BorderPane();
+
+        //Acteurs du jeu
+        Personnage pacman = new Pacman();
+        Personnage fantome = new Fantome();
+        // on positionne le fantôme dans l'angle inférieur droit
+        fantome.setLayoutX(620);
+        fantome.setLayoutY(460);
+        //panneau du jeu
+        Pane jeu = new Pane();
+        jeu.setPrefSize(640, 480);
+        jeu.getChildren().add(pacman);
+        jeu.getChildren().add(fantome);
+        root.setCenter(jeu);
+        //on construit une scene 640 * 480 pixels
+        scene = new Scene(root);
+
+        //Gestion du déplacement du personnage
+        deplacer(pacman, fantome);
+
+        primaryStage.setTitle("... Pac Man ...");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * Permet de gérer les événements de type clavier, pression des touches
+     * pour le j1(up,down, right, left), pour le j2( z,q,s,d)
+     *
+     * @param j1
+     * @param j2
+     */
+    private void deplacer(Personnage j1, Personnage j2) {
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case LEFT:
+                    j1.deplacerAGauche();
+                    break;
+                case RIGHT:
+                    j1.deplacerADroite(scene.getWidth());
+                    break;
+                case UP:
+                    j1.deplacerEnHaut();
+                    break;
+                case DOWN:
+                    j1.deplacerEnBas(scene.getHeight());
+                    break;
+                case Q:
+                    j2.deplacerAGauche();
+                    break;
+                case D:
+                    j2.deplacerADroite(scene.getWidth());
+                    break;
+                case Z:
+                    j2.deplacerEnHaut();
+                    break;
+                case S:
+                    j2.deplacerEnBas(scene.getHeight());
+                    break;
+            }
+            if (j1.estEnCollision(j2)) {
+                Text gameOver = new Text("Game Over");
+                gameOver.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
+                gameOver.setFill(Color.RED);
+                BorderPane.setAlignment(gameOver, Pos.CENTER);
+                root.setCenter(gameOver);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/partie2/PacMan/Obstacle.java
@@ -0,0 +1,6 @@
+package com.example.partie2.PacMan;
+
+import javafx.scene.shape.Rectangle;
+
+public class Obstacle extends Rectangle {
+}
