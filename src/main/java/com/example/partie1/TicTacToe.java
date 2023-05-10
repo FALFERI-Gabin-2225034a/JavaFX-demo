88bab3f4f54b9fec96e5c526fc93bc1b355789d8
GabinFALFERIF22003745 <gabin.falferi@etu.univ-amu.fr>
2023-05-10 11:24:12 +0200

    Création application TicTacToe

--- src/main/java/com/example/partie1/TicTacToe.java
+++ src/main/java/com/example/partie1/TicTacToe.java
@@ -2,10 +2,17 @@ package com.example.partie1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TicTacToe extends Application {
 
@@ -13,22 +20,47 @@ public class TicTacToe extends Application {
     public void start(Stage primaryStage) throws Exception {
         // Création du conteneur principal
         GridPane grille = new GridPane();
+        grille.setGridLinesVisible(true);
+
+        // Création image
+        ArrayList<ImageView> ListeIV = new ArrayList<>();
+        for (int i = 0; i < 9; ++i) {ListeIV.add(new ImageView());};
+
+        ArrayList<Label> ListeLabel = new ArrayList<>();
+        for (int i = 0; i < 9; ++i) {ListeLabel.add(new Label());};
+
+        // Pacage des images
+        Random random = new Random();
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                int nombre = random.nextInt(3);
+                switch (nombre) {
+                    case 1:
+                        ListeIV.get(i*3+j).setImage(new Image(TicTacToe.class.getResource("Croix.png").toString()));
+                        break;
+                    case 2:
+                        ListeIV.get(i*3+j).setImage(new Image(TicTacToe.class.getResource("Rond.png").toString()));
+                        break;
+                    case 3:
+                        ListeIV.get(i*3+j).setImage(new Image(TicTacToe.class.getResource("Vide.png").toString()));
+                        break;
+                    default:
+                        break;
+                }
+                ListeLabel.get(i*3+j).setGraphic(ListeIV.get(i*3+j));
+                grille.add(ListeLabel.get(i*3+j), i, j);
+            }
+        }
 
         // Ajout du conteneur à la scene
         Scene scene = new Scene(grille);
 
         // Ajout de la scene à la fenêtre et changement de ses paramètres (dimensions et titre)
         primaryStage.setScene( scene );
-        primaryStage.setWidth( 800 );
-        primaryStage.setHeight( 600 );
-        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Tic Tac Toe");
 
         // Affichage de la fenêtre
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
